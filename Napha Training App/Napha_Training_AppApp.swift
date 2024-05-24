351fcc664809218d6da832b7316660f74f3236cd
k <chen_kui_jun@s2023.ssts.edu.sg>
2024-05-24 13:50:25 +0800

    Initial Commit

---
+++ Napha Training App/Napha_Training_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Napha_Training_AppApp.swift
+//  Napha Training App
+//
+//  Created by Kui Jun on 24/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Napha_Training_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Napha Training AppUITests/Napha_Training_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Napha_Training_AppUITestsLaunchTests.swift
+//  Napha Training AppUITests
+//
+//  Created by Kui Jun on 24/5/24.
+//
+
+import XCTest
+
+final class Napha_Training_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
